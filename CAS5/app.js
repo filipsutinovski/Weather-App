164a37cd084cc2bed4bac98692c6f555c3d74dba
Filip Sutinovski <filipsutinovski95@hotmail.com>
2020-01-27 19:35:16 +0100

    Adding App Folder CAS5

---
+++ CAS5/app.js
@@ -0,0 +1,9 @@
+// 1. Create navbar with links to 3 of the needed pages (Home-statistics, hourly-weather , about)
+// 2. Add log and application name into navbar
+// 3. Add 3 pages (as divs to HTML)
+// 4. Add js logic for navbar links
+// 5. Create first page details add to html (welcome message and text)
+// 6. Make AJAX call to http://api.openweathermap.org/ (find the appropriate url),
+// get the needed date, aggregate it and display it into html
+// 7. Create 2nd page details (hourly data) get the needed data using AJAX call from the api and display then in a HTML table
+// 8. Create about page details (only html info about us)
